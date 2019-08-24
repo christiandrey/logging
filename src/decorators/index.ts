2400beb9ad21ae4badfcfa31298a707e8ad6e300
Christian Dréy <oluwaseun.adedire@gmail.com>
2019-08-24 05:25:48 +0100

    Add console logger and file logger

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,4 +1,7 @@
+import * as path from "path";
+import * as fs from "fs";
 import { HttpResponse, ApiResponse, ApiResponseError, HttpStatusCodes } from "elf-utils";
+
 export const Logger = (): MethodDecorator => {
 	return (target: Object, propertyName: string, propertyDescriptor: PropertyDescriptor) => {
 		const original = propertyDescriptor.value;
@@ -8,6 +11,67 @@ export const Logger = (): MethodDecorator => {
 				const base = await original.apply(this, args);
 				return base;
 			} catch (error) {
+				return new HttpResponse(
+					new ApiResponse({
+						errors: [
+							new ApiResponseError({
+								message: "An unexpected error occured. Please try again in a few minutes."
+							})
+						]
+					}),
+					HttpStatusCodes.internalServerError
+				);
+			}
+		};
+
+		propertyDescriptor.value = modified;
+
+		return propertyDescriptor;
+	};
+};
+
+export const FileLogger = (): MethodDecorator => {
+	return (target: Object, propertyName: string, propertyDescriptor: PropertyDescriptor) => {
+		const original = propertyDescriptor.value;
+
+		const modified = async function(...args: any[]) {
+			try {
+				const base = await original.apply(this, args);
+				return base;
+			} catch (error) {
+				const filePath = path.resolve(__dirname, "elf.log");
+				const content = `\nFATAL: ${new Date().toISOString()}: ${error}\n`;
+				fs.appendFile(filePath, content, () => {});
+
+				return new HttpResponse(
+					new ApiResponse({
+						errors: [
+							new ApiResponseError({
+								message: "An unexpected error occured. Please try again in a few minutes."
+							})
+						]
+					}),
+					HttpStatusCodes.internalServerError
+				);
+			}
+		};
+
+		propertyDescriptor.value = modified;
+
+		return propertyDescriptor;
+	};
+};
+
+export const ConsoleLogger = (): MethodDecorator => {
+	return (target: Object, propertyName: string, propertyDescriptor: PropertyDescriptor) => {
+		const original = propertyDescriptor.value;
+
+		const modified = async function(...args: any[]) {
+			try {
+				const base = await original.apply(this, args);
+				return base;
+			} catch (error) {
+				console.log("FATAL: ", new Date().toISOString(), ": ", error);
 				return new HttpResponse(
 					new ApiResponse({
 						errors: [
